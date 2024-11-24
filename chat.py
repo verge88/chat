9995c8c9658c239223bbb284db5db7079f37a690
verge88 <157506627+verge88@users.noreply.github.com>
2024-11-24 23:14:21 +0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -44,10 +44,10 @@ def predict_get():
 def predict_post():
     try:
         # Извлечение текста
-        input_text = request.form.get('text', '')
+        #input_text = request.form.get('text', '')
 
-        if not input_text:
-            return jsonify({'error': 'Missing required "text" parameter'}), 400
+        #if not input_text:
+          #  return jsonify({'error': 'Missing required "text" parameter'}), 400
 
         # Извлечение файлов
         uploaded_files = request.files.getlist('files')
@@ -64,7 +64,7 @@ def predict_post():
 
         # Используем Gradio клиент для обработки
         result = client.predict(
-            _input={"files": files_content, "text": input_text},
+            _input={"files": files_content, "text": ''},
             _chatbot=[],
             api_name="/add_text"
         )
@@ -76,7 +76,7 @@ def predict_post():
                     "elem_id": None,
                     "elem_classes": None,
                     "name": None,
-                    "text": input_text,
+                    "text": '',
                     "files": files_content
                 }, None]
             ],
